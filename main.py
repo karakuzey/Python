cf67deb1e1b81574cc6a0a3b6ec1ae70dcd0773c
ABDULLAH KARA <128512287+karakuzey@users.noreply.github.com>
2024-07-13 16:01:35 +0300

    Add files via upload
    in this file I use the information about pandas libary and create more useful csv file which includes the color and how many of them

--- main.py
+++ main.py
@@ -1,93 +1,22 @@
-#data = []
-#with open("weather_data.csv") as file:
-#    rows = file.readlines()
-#    for row in rows:
-#        data.append(row.split("\n"))
-
-#print(data)
-# this way is a bit paintful there is an easier way
-#import csv
-
-#temperatures = []
-
-# with open("weather_data.csv") as file:
-#    rows = csv.reader(file)
-#    counter = 0
-#    for row in rows:
-#        if counter != 0:
-#            temperature = int(row[1])
-#            temperatures.append(temperature)
-#        counter += 1
-#    print(temperatures)
-
-# thats another way of takin tempraetues
-
 import pandas
-# thats an awsome libary has two type of data dataframe and series methods are below
-data = pandas.read_csv("weather_data.csv")
-# print(data)
-# after the next line its more then just awsome
+data = pandas.read_csv("central_park.csv")
+# my task create a csv file wich includes color and how many of them gray , black , Cinnamon
+gray = data[data["Primary Fur Color"] == "Gray"]
+number_of_gray = len(gray)
+print(number_of_gray)
 
-# data = data["temp"]
-# print(data)
-# for i in data:
-#     print(i)
+black = data[data["Primary Fur Color"] == "Black"]
+number_of_black = len(black)
+print(number_of_black)
 
-# there is even easier way to that first how to convert csw to a dictionary
+Cinnamon = data[data["Primary Fur Color"] == "Cinnamon"]
+number_of_Cinnamon = len(Cinnamon)
+print(number_of_Cinnamon)
 
-# data_dictionary = data.to_dict()
-# print(data_dictionary)
-#
-# # now how to take a column as a series just easy as possible its name is series
-#
-# temperatures = data["temp"].to_list()
-# print(temperatures)
-# print(f"{sum(temperatures) / len(temperatures)}")
-# # build in function for average just name is different
-# print(data["temp"].mean())
-# print(data["temp"].max())
-# print(max(temperatures))
-#
-# # against to use data[""] form
-#
-# print(data.condition)
-# print(data["condition"])
-
-# how we read a row of a table
-
-# print(data[data.day == "Monday"])
-
-
-day = data[data.temp == data.temp.max()].day
-print(day)
-
-max_temp = data.temp.max()
-print(max_temp)
-
-monday = data[data.day == "Monday"]
-print(f"day :  {monday.day} , temperature : {monday.temp}")
-
-
-# using pandas lbary to create a dataframe
-
-students_data = {
-    "students" : ["ali","veli","mehmet"],
-    "scores" : [99,98,97]
+central_info = {
+    "color" : ["Gray", "Black", "Cinnamon"],
+    "count" : [number_of_gray,number_of_black,number_of_Cinnamon]
 }
 
-students_infos = pandas.DataFrame(students_data)
-
-print(students_infos)
-
-# creating csv files
-
-# students_infos.to_csv("student_infos.csv")
-
-
-# the days temp = 14
-
-fourteen = data[data.temp == 14]
-print(fourteen)
-
-max_temp = data[data.temp == data.temp.max()].day
-print(max_temp)
+central_park_data = pandas.DataFrame(central_info)
+central_park_data.to_csv("central_park_data.csv")
