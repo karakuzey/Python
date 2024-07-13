2a4cc1b612b095758763ad0c082325b65b05078c
ABDULLAH KARA <128512287+karakuzey@users.noreply.github.com>
2024-07-13 16:59:37 +0300

    Add files via upload
    using the knowledge of pandas and turtle libary I create a game wich we are trying to know us states if we knew it it will be appear at the right place on the map

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
 import pandas
-data = pandas.read_csv("central_park.csv")
-# my task create a csv file wich includes color and how many of them gray , black , Cinnamon
-gray = data[data["Primary Fur Color"] == "Gray"]
-number_of_gray = len(gray)
-print(number_of_gray)
+import turtle
+import time
 
-black = data[data["Primary Fur Color"] == "Black"]
-number_of_black = len(black)
-print(number_of_black)
 
-Cinnamon = data[data["Primary Fur Color"] == "Cinnamon"]
-number_of_Cinnamon = len(Cinnamon)
-print(number_of_Cinnamon)
+screen = turtle.Screen()
+screen.title("US states game")
+img = "blank_states_img.gif"
+screen.addshape(img)
+turtle.shape(img)
 
-central_info = {
-    "color" : ["Gray", "Black", "Cinnamon"],
-    "count" : [number_of_gray,number_of_black,number_of_Cinnamon]
-}
 
-central_park_data = pandas.DataFrame(central_info)
-central_park_data.to_csv("central_park_data.csv")
+city_infos = pandas.read_csv("50_states.csv")
+city_names = city_infos["state"].to_list()
+print(city_names)
+
+game_is_on = True
+
+
+def create_and_send_city(city, x, y):
+    city_place = turtle.Turtle()
+    city_place.hideturtle()
+    city_place.penup()
+    city_place.goto(x, y)
+    city_place.write(city, align="center", font=("Courier", 8, "normal"))
+
+
+while game_is_on:
+    city_name = shape = screen.textinput("Şekil Girişi", "wich city do you wanna try if you wanna quit \n"
+                                                         "type s : ")
+    if city_name in city_names:
+        cor_x = city_infos[city_infos.state == city_name].x.iloc[0]
+        # becasue of cor_x is a pandas series we use iloc its the same thing with x[0} or x.iloc[0]
+        # just because of its a part of pandas libary we use it
+        print(cor_x)
+        cor_y = city_infos[city_infos.state == city_name].y.iloc[0]
+        print(cor_y)
+        create_and_send_city(city_name, cor_x, cor_y)
+        time.sleep(2)
+
+    if city_name == "s":
+        game_is_on = False
+
+screen.exitonclick()
